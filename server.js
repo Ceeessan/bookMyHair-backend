be9ffe2360a512a0374214d680446ee533d41f25
Ceeessan <Cessan__94@hotmail.com>
2025-05-24 00:01:36 +0200

    First version of backend to Book My Hair

---
+++ server.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+app.get('/', (req, res) => {
+    res.send('Frisörbokning backend är igång!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servern kör på port ${PORT}`);
+});
